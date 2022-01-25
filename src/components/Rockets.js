62c8fe03b2e3c45105b52c4f8614b168f4412ffe
Yigit Mersin <yigitmersin@gmail.com>
2022-01-26 01:47:07 +0300

    Add list display & style

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -4,22 +4,28 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocketsReducer);
 
   const displayRockets = rockets.map((rocket) => (
-    <Card key={rocket.id} style={{ width: '18rem' }}>
-      <Card.Img src={rocket.flickrImages} />
-      <Card.Body>
-        <Card.Title>{rocket.rocketName}</Card.Title>.
-        <Card.Text>{rocket.description}</Card.Text>
-        <Button variant="primary">Reserve Rocket</Button>
-      </Card.Body>
-    </Card>
+    <Row key={rocket.id} className="g-0">
+      <Col className="col-2">
+        <Image src={rocket.flickrImages} className="img-thumbnail" />
+      </Col>
+      <Col className="col-10">
+        <Card.Body>
+          <Card.Title className="fw-bold">{rocket.rocketName}</Card.Title>
+          <Card.Text>{rocket.description}</Card.Text>
+          <Button>Reserve Rocket</Button>
+        </Card.Body>
+      </Col>
+    </Row>
   ));
 
-  return displayRockets;
+  return <Container fluid>{displayRockets}</Container>;
 };
 export default Rockets;
